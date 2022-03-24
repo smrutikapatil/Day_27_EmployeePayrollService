070cf0cb71e94afd21ef6871b20d17c760f79221
smrutikapatil <smruti.patil17@gmail.com>
2022-03-24 21:10:54 +0530

    Added- Read white employee payroll to cansole

---
+++ src/main/java/EmployeePayrollData.java
@@ -0,0 +1,41 @@
+public class EmployeePayrollData {
+
+    public int employeeId;
+    public String employeeName;
+    public double salary;
+
+    public EmployeePayrollData(int employeeId, String employeeName, double salary) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.salary = salary;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Id : " + employeeId + "\t" + "Name : " + employeeName + "\t" + "Salary : " + salary;
+    }
+}
--- src/main/java/EmployeePayrollService.java
+++ src/main/java/EmployeePayrollService.java
@@ -1,5 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class EmployeePayrollService {
+    Scanner SC = new Scanner(System.in);
+
+    public enum Ioservice {CONSALE_IO, FILE_IO, DB_IO, REST_IO}
+
+    private List<EmployeePayrollData> employeePayrollList;
+
+    public EmployeePayrollService() {
+        this.employeePayrollList = new ArrayList<EmployeePayrollData>();
+    }
+
+    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+        this.employeePayrollList = employeePayrollList;
+    }
+
     public static void main(String[] args) {
-        System.out.println("welcome to employee payroll service");
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();
+    }
+
+    public void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.println("Enter Employee Id:");
+        int employeeId = consoleInputReader.nextInt();
+        System.out.println("Enter Employee Name:");
+        String employeeName = consoleInputReader.next();
+        System.out.println("Enter Employee Salary:");
+        double employeeSalary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(employeeId, employeeName, employeeSalary));
+    }
+
+    public void writeEmployeePayrollData() {
+        System.out.println("\nWriting Employee Payroll Roaster to Cansole\n" + employeePayrollList);
     }
 }
